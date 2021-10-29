9d4424b0605cc1e0bd5ee113628823c9d7c6304d
Amaan-09 <79093439+Amaan-09@users.noreply.github.com>
2021-10-29 18:24:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -25,6 +25,17 @@ function modelLoaded()
 
 function draw(){
     image(video, 0, 0, 600, 500);
+    fill("#FF0000");
+    stroke("#FF0000");
+    if(scoreLeftWrist > 0.2)
+    {
+    circle(leftWristX, leftWristY, 20);
+    InNumberleftWristY = Number(leftWristY);
+    remove_decimals= floor(InNumberleftWristY);
+    volume = remove_decimals/500;
+    document.getElementById("volume").innerHTML="Volume = "+volume;
+    song.setVolume(volume);
+    }
 }
 
 function play(){
@@ -38,6 +49,9 @@ function gotPoses(results)
    if(results.length > 0)
    {
        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = "+ scoreLeftWrist);
+
        leftWristX= results[0].pose.leftWrist.x;
        leftWristY= results[0].pose.rightWrist.y;
         console.log("leftWristX = "+leftWristX+ "leftWristY = " +leftWristY);
